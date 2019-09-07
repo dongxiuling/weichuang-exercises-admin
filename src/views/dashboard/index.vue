f9cf3ccaed78aae6272d2a28a64e99bab5814b07
dongxiuling <644961699@qq.com>
2019-09-07 10:14:03 +0800

    bug fix

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">
+        <div class="title">欢迎来到题库后台管理系统 </div>
+        <div class="imgs">
+            <img src="./../../assets/1.png" alt="">
+            <img src="./../../assets/2.png" alt="">
+        </div>
+    </div>
   </div>
 </template>
 
@@ -25,6 +31,20 @@ export default {
   &-text {
     font-size: 30px;
     line-height: 46px;
+    .title{
+      color:#606266;
+       text-align: center;
+       height:40px;
+       line-height: 40px;
+       font-size:24px;
+    }
+    .imgs{
+       text-align: center;
+       margin-top:70px;
+       img{
+          width:280px;
+       }
+    }
   }
 }
 </style>
